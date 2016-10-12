d8bfcb335b7847675ccd8851c126033525a0aec2
Gonzalez <JuanPablo.Gonzalez@Sovos.com>
2016-10-12 16:27:36 -0300

    Learning State.

--- app/components/layout/Header.js
+++ app/components/layout/Header.js
@@ -7,11 +7,11 @@ import React, { Component } from 'react';
 import AppBar from 'material-ui/AppBar';
  
 class Header extends Component{
-render(){
-return(
-<AppBar title="Welcome to Sovos ReactJS training!" />
-);
- 	}
- }
+  render(){
+    return(
+      <AppBar title="Welcome to Sovos ReactJS training!" />
+    );
+  }
+}
 
 export default Header;
--- app/components/people/list.js
+++ app/components/people/list.js
@@ -5,22 +5,22 @@ import UserDetail from './detail';
 class UserList extends Component{
 	constructor(){
     super();
-		this.usersList = [
-			{id:1, name:'juan pablo gonzalez',address:'111 Lomas st.'},
-			{id:2, name:'juan carlos gonzalez',address:'222 Lomas st.'},
-			{id:3, name:'juan carlos masaguer',address:'333 Lomas st.'},
-			{id:4, name:'rodrigo reymond',address:'444 Lomas st.'},
-			{id:5, name:'viginia mansila',address:'555 Lomas st.'},
-			{id:6, name:'peter capusoto',address:'666 Lomas st.'},
-			{id:7, name:'adrian veliz',address:'777 Lomas st.'}
-		];
-    this.selectedUser = {id:1, name:'No name', address:'No address'};
+    this.state = {
+      usersList: [
+        {id:1, name:'juan pablo gonzalez',address:'111 Lomas st.'},
+        {id:2, name:'juan carlos gonzalez',address:'222 Lomas st.'},
+        {id:3, name:'juan carlos masaguer',address:'333 Lomas st.'},
+        {id:4, name:'rodrigo reymond',address:'444 Lomas st.'},
+        {id:5, name:'viginia mansila',address:'555 Lomas st.'},
+        {id:6, name:'peter capusoto',address:'666 Lomas st.'},
+        {id:7, name:'adrian veliz',address:'777 Lomas st.'}
+      ],
+      selectedUser: {id:1, name:'No name', address:'No address'}
+    };
 	}
 
   getUserDetails = (user) => {
-    this.selectedUser = user;
-    alert('Current value of selectedUser '+this.selectedUser.name);
-
+    this.setState({selectedUser:user});
   }
 
 	render(){
@@ -28,14 +28,14 @@ class UserList extends Component{
       <div>
         <ul>
           {
-            this.usersList.map((user) =>{
+            this.state.usersList.map((user) =>{
               return (
                 <li key={user.id} onClick={this.getUserDetails.bind(this, user)} >{user.name}</li>
               );
             })
           }
         </ul>
-        <UserDetail user={this.selectedUser}/>
+        <UserDetail user={this.state.selectedUser}/>
       </div>
 		);
 	}
