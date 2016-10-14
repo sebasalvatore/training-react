987f8349ab697814738a46df8f5d0ab3bde516b2
Gonzalez <JuanPablo.Gonzalez@Sovos.com>
2016-10-14 08:13:05 -0300

    working with states

--- app/components/layout/Header.js
+++ app/components/layout/Header.js
@@ -5,11 +5,44 @@ In this class we learn how to create a component that use another component (in 
 
 import React, { Component } from 'react';
 import AppBar from 'material-ui/AppBar';
- 
+import Drawer from 'material-ui/Drawer';
+import MenuItem from 'material-ui/MenuItem';
+
 class Header extends Component{
+  constructor(){
+    super();
+    this.state = {
+      open: false
+    };
+  }
+
+  toogleMenu(){
+    this.setState({
+      open: !this.state.open
+    });
+  }
+
   render(){
     return(
-      <AppBar title="Welcome to Sovos ReactJS training!" />
+      <div>
+        <AppBar 
+          onLeftIconButtonTouchTap={ () => this.toogleMenu() }
+          title="Welcome to Sovos ReactJS training!" 
+        />
+        <Drawer
+          docked={ true }
+          open={ this.state.open }
+          width={ 250 }
+          openSecondary={true}
+        >
+          <AppBar 
+            title="Menu" 
+            showMenuIconButton={false}
+          />
+          <MenuItem> Menu 1</MenuItem>
+          <MenuItem> Menu 2</MenuItem>
+        </Drawer>
+      </div>
     );
   }
 }
